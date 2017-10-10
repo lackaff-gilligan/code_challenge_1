cefb149c352ac913d089166b582482f26a06e793
Kevin Lackaff-Gilligan <kevin.lackaff.gilligan@gmail.com>
2017-10-10 08:32:05 -0500

    fixed recalling function

--- client.js
+++ client.js
@@ -14,19 +14,30 @@ function appendDiv(){
   //add one to the clicks variable each time Generate btn is clicked
     clicks += 1;
   //add a div element to the body
-  $('body').append('<div class="myDiv red"></div>');
-appendThingsToDiv();
+  $('body').append('<div class="myDiv"><p>' + clicks + '</p><button class="swap">Swap</button><button class="delete">Delete</button></div>');
+//appendThingsToDiv();
 }
 
-function appendThingsToDiv(){
-  //add a <p> element that displays the tracked clicks
-$('.myDiv').append('<p>' + clicks + '</p>');
-// add the swap and delete buttons
-$('.myDiv').append('<button class="swap">Swap</button>');
-$('.myDiv').append('<button class="delete">Delete</button>');
-}
+// function appendThingsToDiv(){
+//   //add a <p> element that displays the tracked clicks
+// $('.myDiv').append('<p>' + clicks + '</p>');
+// // add the swap and delete buttons
+// $('.myDiv').append('<button class="swap">Swap</button>');
+// $('.myDiv').append('<button class="delete">Delete</button>');
+// }
 
 //clicking swap button changes the div's background-color to yellow
+// function swapClicked(){
+// var theDiv = $(this).parent();
+//   if(theDiv.css('background-color') === 'red') {
+//     theDiv.css('background-color', 'yellow');
+//   }
+//
+//   if(theDiv.css('background-color') === 'yellow') {
+//     theDiv.css('background-color', 'red');
+//   }
+// }
+
 function swapClicked(){
   $(this).parent().css('background-color', 'yellow');
 }
